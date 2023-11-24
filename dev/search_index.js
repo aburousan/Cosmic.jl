9ed244510a24ec2b985a43c6c20f5ca98e4ff672
Documenter.jl <documenter@juliadocs.github.io>
2023-11-24 19:25:14 +0000

    build based on b20cfba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = cosmic","category":"page"},{"location":"#cosmic","page":"Home","title":"cosmic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for cosmic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [cosmic]","category":"page"}]
+}
